05573e5588a21fc00811b8d08cf6add7494b1369
crissisueta <cristianwalter30acd@gmail.com>
2026-03-04 15:26:20 -0300

    change database

--- fisio_project/settings.py
+++ fisio_project/settings.py
@@ -66,13 +66,11 @@ WSGI_APPLICATION = 'fisio_project.wsgi.application'
 
 # Database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 
