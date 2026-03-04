dca195efd9914d75a9acb1981843954318de682b
crissisueta <cristianwalter30acd@gmail.com>
2026-03-04 13:57:55 -0300

    setu pchanges for deploy

--- fisio_project/settings.py
+++ fisio_project/settings.py
@@ -9,10 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-change-this-in-production'
+import os
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ['*']
 
